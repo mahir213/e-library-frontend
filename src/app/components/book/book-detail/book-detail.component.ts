79c2300b080850b1fe40925fbe7a55711c42ef95
mahir213 <hajdarevicm10@gmail.com^>
2025-01-05 16:14:45 +0100

    forms

--- src/app/components/book/book-detail/book-detail.component.ts
+++ src/app/components/book/book-detail/book-detail.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+// filepath: /c:/Users/Mahir-Ideapad 3/Desktop/E-library/frontend/e-library-frontend/src/app/components/book/book-detail/book-detail.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-book-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './book-detail.component.html',
-  styleUrl: './book-detail.component.css'
+  styleUrls: ['./book-detail.component.css']
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit {
+  book: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const bookId = this.route.snapshot.paramMap.get('id');
+    
+    this.book = {
+      id: bookId,
+      title: 'Sample Book Title',
+      author: 'Sample Author',
+      published_date: '2023-01-01',
+      isbn: '123-456-789',
+      description: 'This is a sample description of the book.',
+      cover_image_url: 'assets/sample-book-cover.jpg'
+    };
+  }
 }
